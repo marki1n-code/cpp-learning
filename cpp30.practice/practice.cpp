67620cc69695535d1b5f2d85860500b7103031ad
marki1n-code <mramur47@gmail.com>
2026-01-28 22:46:26 +0200

    Refactor code structure for improved readability and maintainability

---
+++ cpp30.practice/practice.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+void showBalance(double balance);
+double deposit();
+double withdraw(double balance);
+
+int main()
+{
+
+    double balance = 0;
+    int choice = 0;
+
+    return 0;
+}
